238135bd310071522f13eb8eb330146ce27e021b
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-21 18:51:15 +0200

    feat: added newpostserializer to fix the author issue

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,22 @@
+from django.db.models import fields
 from rest_framework import serializers
-
+from django.contrib.auth import get_user_model
 from .models import Post
 
 class PostsSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all())
+    class Meta:
+        model = Post
+        fields = ('__all__')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('username',)
+
+class NewPostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(many = False)
 
     class Meta:
         model = Post
-        fields = ('id','title', 'content', 'author', 'hashtags', 'time_added', 'time_updated', 'notes')
+        fields = "__all__"
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,18 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
-from .serializers import PostsSerializer
+from .serializers import NewPostSerializer, PostsSerializer
 from .models import Post
 
 class PostsAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostsSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = NewPostSerializer(queryset, many = True)
+        return Response(serializer.data)
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
